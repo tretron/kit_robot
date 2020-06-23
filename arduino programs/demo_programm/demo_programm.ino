3441d691eaa563e2f8da7758f33e02becdd89af7
l roos <betrayedcover@gmail.com>
2020-06-23 21:56:59 +0200

    added inputs and outputs declaration

--- arduino programs/demo_programm/demo_programm.ino
+++ arduino programs/demo_programm/demo_programm.ino
@@ -1,20 +1,41 @@
-#define button_tool_rotate_+ PIN
-#define button_tool_rotate_- PIN
-#define button_wrist_+ PIN
-#define button_writst_- PIN
-#define stepper_tool_rotate_DIR
-#define stepper_tool_rotate_PULSE
-#define stepper_wrist_DIR
-#define stepper_wrist_PULSE
+#define button_tool_rotate_pos 1
+#define button_tool_rotate_neg 2
+#define button_wrist_pos 3
+#define button_writst_neg 4
+#define stepper_tool_rotate_DIR 5
+#define stepper_tool_rotate_PULSE 6
+#define stepper_wrist_DIR 7
+#define stepper_wrist_PULSE 8
 
-
+const int inputs[] = {button_tool_rotate_pos, button_tool_rotate_neg,button_wrist_pos,button_writst_neg};
+const int stepper_joints[][2] = { {stepper_tool_rotate_DIR,stepper_tool_rotate_PULSE},
+                                  {stepper_wrist_DIR,stepper_wrist_PULSE}};
 
 void setup() {
-  // put your setup code here, to run once:
+  set_input_pullup(inputs);
+  set_stepper_outputs(stepper_joints);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void set_input_pullup(int pins[]){
+  for(int i=0; i<= (sizeof(pins)-1); i++){
+    pinMode(pins[i], INPUT_PULLUP);
+  }
+}
+
+void set_stepper_outputs(int pins[][2]){
+  for(int j=0; j<= (sizeof(pins)-1); j++){
+    set_outputs(pins[j]);
+  }
+}
+
+void set_outputs(int pins[]){
+  for(int i=0; i<= (sizeof(pins)-1); i++){
+    pinMode(pins[i], OUTPUT);
+  }
 }
