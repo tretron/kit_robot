17042b37bdad857e954e9cdf6a5a92c85ad425e4
l roos <betrayedcover@gmail.com>
2020-06-23 21:41:27 +0200

    iunit commit, added the defines needed for a least viable demo product

---
+++ arduino programs/demo_programm/demo_programm.ino
@@ -0,0 +1,20 @@
+#define button_tool_rotate_+ PIN
+#define button_tool_rotate_- PIN
+#define button_wrist_+ PIN
+#define button_writst_- PIN
+#define stepper_tool_rotate_DIR
+#define stepper_tool_rotate_PULSE
+#define stepper_wrist_DIR
+#define stepper_wrist_PULSE
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
